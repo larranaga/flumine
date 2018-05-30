983fcc396bb63b76ef49d242719bd7c3880f35b9
Andres Mauricio Rondon <amrondonp@gmail.com>
2018-05-29 23:43:06 -0500

    Add fisrt attempt to manage functions

---
+++ src/examples/impact.ts
@@ -0,0 +1,48 @@
+import * as ts from 'typescript';
+
+const printer: ts.Printer = ts.createPrinter();
+
+const source: string = `function someFunction() { alert("Hello!!"); }`;
+
+const transformer = <T extends ts.Node>(context: ts.TransformationContext) => (rootNode: T) => {
+    function visit(node: ts.Node): ts.Node {
+        node = ts.visitEachChild(node, visit, context);
+
+        if(node.kind === ts.SyntaxKind.FunctionDeclaration) {
+            const declaration = node as ts.FunctionDeclaration;
+                        
+            return ts.createFunctionDeclaration(
+                declaration.decorators,
+                declaration.modifiers,
+                declaration.asteriskToken,
+                declaration.name,
+                declaration.typeParameters,
+                declaration.parameters,
+                declaration.type,
+                declaration.body
+            )
+        }
+
+        return node;
+    }
+
+    return ts.visitNode(rootNode, visit);
+}
+
+const sourceFile: ts.SourceFile = ts.createSourceFile(
+    'test.ts', source, ts.ScriptTarget.ES2015, true, ts.ScriptKind.TS
+);
+
+console.log(printer.printFile(sourceFile));
+
+// Options may be passed to transform
+const result: ts.TransformationResult<ts.SourceFile> = ts.transform<ts.SourceFile>(
+    sourceFile, [transformer]
+);
+
+const transformedSourceFile: ts.SourceFile = result.transformed[0];
+
+
+console.log(printer.printFile(transformedSourceFile));
+
+result.dispose();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './examples/createAdd';
-import './examples/createArrow';
-import './examples/printVar';
+// import './examples/createAdd';
+// import './examples/createArrow';
+// import './examples/printVar';
 
-import './examples/loggingTransform';
-import './examples/transform';
+// import './examples/loggingTransform';
+// import './examples/transform';
+import './examples/impact';
